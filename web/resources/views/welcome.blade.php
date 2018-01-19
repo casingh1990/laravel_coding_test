a5a8c54e308e05426785df0c67f4c79c4080cb55
casingh1990 <32516406+casingh1990@users.noreply.github.com>
2018-01-18 20:38:34 -0500

    Display of table heading working

--- web/resources/views/welcome.blade.php
+++ web/resources/views/welcome.blade.php
@@ -105,9 +105,33 @@
                            col: "id",
                            asc: true
                        },
-                   }
-               }
-           );
+                   },
+                   computed: {
+                       isActived: function() {
+                           return this.pagination.current_page;
+                       },
+                       pagesNumber: function () {
+                           if (!this.pagination.to) {
+                               return [];
+                           }
+                           var from = this.pagination.current_page - this.offset;
+                           if (from < 1) {
+                               from = 1;
+                           }
+                           var to = from + (this.offset * 2);
+                           if (to >= this.pagination.last_page) {
+                               to = this.pagination.last_page;
+                           }
+                           var pagesArray = [];
+                           while (from <= to) {
+                               pagesArray.push(from);
+                               from++;
+                           }
+                           return pagesArray;
+                       }//end pagesNumber
+                   }//end computed
+               }//end Vue App
+           );//end Vue App
         </script>
     </body>
 </html>
