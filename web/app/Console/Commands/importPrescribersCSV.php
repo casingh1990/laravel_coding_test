7874ead8c825323a6a57407954f0a638bd57b2cd
casingh1990 <32516406+casingh1990@users.noreply.github.com>
2018-01-18 19:25:21 -0500

    Added Error Checking

--- web/app/Console/Commands/importPrescribersCSV.php
+++ web/app/Console/Commands/importPrescribersCSV.php
@@ -3,9 +3,19 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
 
 class importPrescribersCSV extends Command
 {
+    /**
+     * @var string Filename
+     **/
+    private $file;
+    /**
+     * @var string error values
+     **/
+    private $errors;
+
     /**
      * The name and signature of the console command.
      *
@@ -18,7 +28,7 @@ class importPrescribersCSV extends Command
      *
      * @var string
      */
-    protected $description = 'Imports a CSV File of Prescribers.\n Columns supported are:\n
+    protected $description = 'Imports a CSV File of Prescribers. Columns supported are:
     id, npi, name, email, password, phone, phone_extension, fax, role, is_admin,created_at,updated_at';
 
     /**
@@ -29,6 +39,7 @@ class importPrescribersCSV extends Command
     public function __construct()
     {
         parent::__construct();
+        $this->errors = "";
     }
 
     /**
@@ -38,6 +49,41 @@ class importPrescribersCSV extends Command
      */
     public function handle()
     {
+        //Get Name of file
+        $this->file = $this->argument("file");
 
+        //validate input
+        if ($this->validate()){
+            return $this->outputError();
+        }
     }
+
+    private function validate(){
+        $fs = new Filesystem();
+        if (!$fs->exists($this->file)){
+            $this->addError("File {$this->file} does not exist");
+        }
+        else if ($fs->extension($this->file) !== "csv"){
+            $this->addError("File must be csv");
+        }
+        return $this->hasErrors();
+    }
+
+    private function hasErrors(){
+        return ($this->errors !== "");
+    }
+
+    /**
+     * Outputs any error to the console
+     * @return mixed
+     */
+    private function outputError(){
+        return $this->error($this->errors);
+    }
+
+    private function addError($error_message){
+        $this->errors .= ($this->errors === "")?$error_message:("\n" . $error_message);
+    }
+
+
 }
