a7556b8afb33cd6fee01c1932833ae699397d230
casingh1990 <32516406+casingh1990@users.noreply.github.com>
2018-01-18 20:33:52 -0500

    Vue Template and Starter

--- web/resources/views/welcome.blade.php
+++ web/resources/views/welcome.blade.php
@@ -10,34 +10,104 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link href="/css/bootstrap.css" rel="stylesheet" type="text/css" />
-        
+        <script src="/js/vue.js"></script>
+        <script src="/js/jquery-2.2.4.min.js"></script>
+
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+        <div style="text-shadow: 1px 1px 3px gray;" class="h1 text-center">
+            PRESCRIBERS
+        </div>
+        <div class="flex-center">
+            <div class="container">
+                <div class="col-md-10 col-md-offset-1">
+                    <div id="app">
+                        <table class="table">
+                           <thead class="thead-dark">
+                               <tr>
+                                   <th v-on:click="sortByColumn('id');" id="col-id" scope="col">
+                                       # <span v-if="columns.col = 'id'"><span v-if="columns.asc">/|\</span><span v-else>\|/<span></span>
+                                   </th>
+                                   <th v-on:click="sortByColumn('npi');" id="col-npi" scope="col">
+                                       NPI
+                                   </th>
+                                   <th v-on:click="sortByColumn('name');" id="col-name" scope="col">
+                                       Name
+                                   </th>
+                                   <th v-on:click="sortByColumn('email');" id="col-email" scope="col">
+                                       Email
+                                   </th>
+                                   <th v-on:click="sortByColumn('phone');" id="col-phone" scope="col">
+                                       Phone
+                                   </th>
+                                   <th v-on:click="sortByColumn('fax');" id="col-fax" scope="col">
+                                       Fax
+                                   </th>
+                                   <th v-on:click="sortByColumn('role');" id="col-role" scope="col">
+                                       Role
+                                   </th>
+                               </tr>
+                           </thead>
+                           <tbody>
+                               <tr v-for="prescriber in items">
+                                   <td scope="row" >@{{ prescriber.id }}</td>
+                                   <td>@{{ prescriber.npi }}</td>
+                                   <td>@{{ prescriber.name }}</td>
+                                   <td>@{{ prescriber.email }}</td>
+                                   <td>@{{ prescriber.phone }}</td>
+                                   <td>@{{ prescriber.fax }}</td>
+                                   <td>
+                                       @{{ prescriber.role }}
+                                       @{{ prescriber.is_admin }}
+                                   </td>
+                               </tr>
+                           </tbody>
+                       </table>
+                       <nav>
+                           <ul class="pagination">
+                               <li v-if="pagination.current_page > 1">
+                                   <a href="#" aria-label="Previous"
+                                      @click.prevent="changePage(pagination.current_page - 1)">
+                                       <span aria-hidden="true">&laquo;</span>
+                                   </a>
+                               </li>
+                               <li v-for="page in pagesNumber"
+                                   v-bind:class="[ page == isActived ? 'active' : '']">
+                                   <a href="#"
+                                      @click.prevent="changePage(page)">@{{ page }}</a>
+                               </li>
+                               <li v-if="pagination.current_page < pagination.last_page">
+                                   <a href="#" aria-label="Next"
+                                      @click.prevent="changePage(pagination.current_page + 1)">
+                                       <span aria-hidden="true">&raquo;</span>
+                                   </a>
+                               </li>
+                           </ul>
+                       </nav>
                 </div>
             </div>
         </div>
+        <script>
+            new Vue({
+                   el: '#app',
+                   data: {
+                       pagination: {
+                           total: 100,
+                           lastPage: 100,
+                           per_page: 7,
+                           from: 1,
+                           to: 0,
+                           current_page: 1
+                       },
+                       offset: 4,// left and right padding from the pagination <span>
+                       items: [],
+                       columns: {
+                           col: "id",
+                           asc: true
+                       },
+                   }
+               }
+           );
+        </script>
     </body>
 </html>
